b93b7a2b131c851103fefa931d0de6558d778736
whz <whz857647673@126.com>
2022-06-17 11:04:55 +0800

    左右同步滚动实现功能，利用timeout异步处理解决dom渲染问题

--- src/rengwu/Rengwu.js
+++ src/rengwu/Rengwu.js
@@ -50,40 +50,43 @@ function Rengwu(props) {
         width:'500px',
         borderRight:"1px solid rgb(220,220,220)",
         padding:"14px",
-        height:'100%'
+        display:'flex',
+        flexDirection:'column'
     }}>
         <div style={{color:"rgb(29, 129, 240)",cursor:"pointer"}}>+ <b style={{
             fontSize:"14px"
         }} onClick={handleClick}>添加任务</b>
         </div>
-        <div style={{marginLeft:"5px",marginTop:"28px"}}>
-             <ul>
-                {
-                props.state.List.map((item,key1)=>
-                <li key={key1} style={{marginBottom:'20px'}}>
-                {
-                  item.children?<div><div style={{display:'inline-block',cursor:"pointer"}} onClick={handlekong}><i style={{color:'#c2c2c2',marginRight:'10px'}}>{!props.state.iskong?'▼':'▶'}</i><span style={{color:'#c2c2c2',marginRight:'8px'}}>{key1+1})</span>
-                  {item.main}
-                  </div>
-                  <div style={{display:'inline-block',verticalAlign:"middle",float:"right"}}>
-                    <span style={{float:"right",clear:'both'}}>{time(item.start,item.end,item.daystart,item.dayend,item.day)}<img src={duck} style={{width:'24px',height:'24px',border:"1px solid white",borderRadius:'50%',verticalAlign:"middle",marginLeft:'10px'}}></img></span>
-                  </div>
-                  <div className={props.state.iskong===true?"kong":""}>
-                    <ul>
-                      {item.children.map((data,key)=><li key={key} style={{marginLeft:'28px',marginTop:"20px"}}><i style={{color:'#c2c2c2',marginRight:'10px'}}>●</i><span style={{color:'#c2c2c2',marginRight:'8px'}}>{key1+1}.{key+1})</span>{data.main}<img src={duck} style={{width:'24px',height:'24px',border:"1px solid white",borderRadius:'50%',verticalAlign:"middle",marginLeft:'10px',float:"right"}}></img><span style={{float:"right"}}>{time(data.start,data.end,data.daystart,data.dayend,data.day)}</span></li>)}
-                    </ul>
-                  </div>
-                  </div>:<div><div style={{display:'inline-block'}}><i style={{color:'#c2c2c2',marginRight:'10px'}}>●</i><span style={{color:'#c2c2c2',marginRight:'8px'}}>{key1+1})</span>
-                  {item.main}
-                  </div>
-                  <div style={{display:'inline-block',verticalAlign:"middle",float:"right"}}>
-                    <span style={{float:"right",clear:'both'}}>{time(item.start,item.end,item.daystart,item.dayend,item.day)}<img src={duck} style={{width:'24px',height:'24px',border:"1px solid white",borderRadius:'50%',verticalAlign:"middle",marginLeft:'10px'}}></img></span>
-                  </div></div>
-                }
-                </li>
-                )}
-            </ul>
-        </div>
+        
+          <div style={{marginLeft:"5px",marginTop:"28px",flex:'1',overflow:'scroll'}} className='zuo'>
+              <ul style={{height:'100%'}}>
+                  {
+                  props.state.List.map((item,key1)=>
+                  <li key={key1} style={{marginBottom:'20px',height:'20px'}}>
+                  {
+                    item.children?<div><div style={{display:'inline-block',cursor:"pointer"}} onClick={handlekong}><i style={{color:'#c2c2c2',marginRight:'10px'}}>{!props.state.iskong?'▼':'▶'}</i><span style={{color:'#c2c2c2',marginRight:'8px'}}>{key1+1})</span>
+                    {item.main}
+                    </div>
+                    <div style={{display:'inline-block',verticalAlign:"middle",float:"right"}}>
+                      <span style={{float:"right",clear:'both'}}>{time(item.start,item.end,item.daystart,item.dayend,item.day)}<img src={duck} style={{width:'24px',height:'24px',border:"1px solid white",borderRadius:'50%',verticalAlign:"middle",marginLeft:'10px'}}></img></span>
+                    </div>
+                    <div className={props.state.iskong===true?"kong":""}>
+                      <ul>
+                        {item.children.map((data,key)=><li key={key} style={{marginLeft:'28px',marginTop:"20px"}}><i style={{color:'#c2c2c2',marginRight:'10px'}}>●</i><span style={{color:'#c2c2c2',marginRight:'8px'}}>{key1+1}.{key+1})</span>{data.main}<img src={duck} style={{width:'24px',height:'24px',border:"1px solid white",borderRadius:'50%',verticalAlign:"middle",marginLeft:'10px',float:"right"}}></img><span style={{float:"right"}}>{time(data.start,data.end,data.daystart,data.dayend,data.day)}</span></li>)}
+                      </ul>
+                    </div>
+                    </div>:<div><div style={{display:'inline-block'}}><i style={{color:'#c2c2c2',marginRight:'10px'}}>●</i><span style={{color:'#c2c2c2',marginRight:'8px'}}>{key1+1})</span>
+                    {item.main}
+                    </div>
+                    <div style={{display:'inline-block',verticalAlign:"middle",float:"right"}}>
+                      <span style={{float:"right",clear:'both'}}>{time(item.start,item.end,item.daystart,item.dayend,item.day)}<img src={duck} style={{width:'24px',height:'24px',border:"1px solid white",borderRadius:'50%',verticalAlign:"middle",marginLeft:'10px'}}></img></span>
+                    </div></div>
+                  }
+                  </li>
+                  )}
+              </ul>
+          </div>
+        
     </div>
   )
 }
